68c6d7795a0e7ed80772aff189ec18bf74f09fe1
Julien Letessier <julien.letessier@gmail.com>
2013-09-01 22:37:06 +0100

    Fixes point coords ↔ geohash updating

--- lib/tsuga/model/point.rb
+++ lib/tsuga/model/point.rb
@@ -5,7 +5,7 @@ module Tsuga::Model
   # 
   # Concretions have the following accessors:
   # - :geohash (64-bit integer)
-
+  # 
   # - :lat (float, -90..90)
   # - :lng (float, -180..180)
   # 
@@ -27,54 +27,92 @@ module Tsuga::Model
     end
 
 
-    def geohash=(value)
-      @_latlng = nil
-      super
-    end
-
-    def set_coords(lat, lng)
-      raise ArgumentError, 'bad lng' unless (-180.0 ... 180.0).include?(lng)
-      raise ArgumentError, 'bad lat' unless ( -90.0 ...  90.0).include?(lat)
-
-      self.geohash = _geohash_from_latlng(lat, lng)
+    def geohash(autoupdate=true)
+      autoupdate ? (super() || _update_geohash) : super()
     end
 
 
-    def lat
-      _latlng.first
+    def geohash=(value, dirty=true)
+      self.lat = self.lng = nil if value && dirty
+      super(value)
     end
 
 
-    def lng
-      _latlng.last
+    def lat(autoupdate=true)
+      autoupdate ? (super() || _update_latlng.first) : super()
+    end
+
+
+    def lng(autoupdate=true)
+      autoupdate ? (super() || _update_latlng.last) : super()
+    end
+
+
+    def lat=(value, dirty=true)
+      self.geohash = nil if value && dirty
+      super(value)
+    end
+
+
+    def lng=(value, dirty=true)
+      self.geohash = nil if value && dirty
+      super(value)
+    end
+
+
+    def set_coords(_lat, _lng)
+      _validate_latlng(_lat, _lng)
+
+      self.lat = _lat
+      self.lng = _lng
+      self
+    end
+
+
+    def inspect
+      "<%s lat:%s lng:%s geohash:%s>" % [
+        self.class.name.gsub(/.*::/, ''),
+        lat(false) ? ("%.3f" % lat(false)) : 'nil',
+        lng(false) ? ("%.3f" % lng(false)) : 'nil',
+        geohash(false) ? ("%016x" % geohash(false)) : 'nil'
+      ]
     end
 
 
     private
 
 
-    def _latlng
-      @_latlng ||= _latlng_from_geohash
+    def _validate_latlng(_lat, _lng)
+      raise ArgumentError, 'bad lat' unless ( -90.0 ...  90.0).include?(_lat)
+      raise ArgumentError, 'bad lng' unless (-180.0 ... 180.0).include?(_lng)
     end
 
 
     # Convert the geohash into lat/lng
-    def _latlng_from_geohash
+    def _update_latlng
+      geohash = self.geohash(false)
+      return [nil,nil] if geohash.nil?
       raise ArgumentError, 'bad hash' unless (0 ... (1<<64)).include?(geohash)
 
       lat,lng = _deinterleave_bits(geohash)
-
       lat = lat * 180.0 / (1<<32) -  90.0
       lng = lng * 360.0 / (1<<32) - 180.0
+      self.send(:lat=, lat, false)
+      self.send(:lng=, lng, false)
 
       [lat, lng]
     end
 
 
-    def _geohash_from_latlng(lat, lng)
+    def _update_geohash
+      lat = self.lat(false)
+      lng = self.lng(false)
+      return nil if lat.nil? || lng.nil?
+      _validate_latlng(lat, lng)
       normalized_lat = ((lat +  90.0) * (1<<32) / 180.0).to_i
       normalized_lng = ((lng + 180.0) * (1<<32) / 360.0).to_i
-      _interleave_bits(normalized_lat, normalized_lng)
+      geohash = _interleave_bits(normalized_lat, normalized_lng)
+      self.send(:geohash=, geohash, false)
     end
 
 
@@ -83,12 +121,14 @@ module Tsuga::Model
       (_interleave_bits_16b(a & 0xffff, b & 0xffff))
     end
 
+
     def _deinterleave_bits(z)
       x_hi, y_hi = _deinterleave_bits_16b(z >> 32)
       x_lo, y_lo = _deinterleave_bits_16b(z & 0xFFFFFFFF)
 
       [((x_hi << 16) | x_lo), ((y_hi << 16) | y_lo)]
     end
+
 
     Magic = [0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF]
 
@@ -115,6 +155,7 @@ module Tsuga::Model
       [_even_bits(z), _even_bits(z >> 1)]
     end
 
+
     def _even_bits(z)
       x = z & 0x55555555
       x = (x ^ (x >>  1)) & 0x33333333
@@ -125,7 +166,15 @@ module Tsuga::Model
   end
 
 
-  class Point < Struct.new(:geohash)
+  class Point
+    module Fields
+      attr_accessor :lat, :lng, :geohash
+    end
+    include Fields
     include PointTrait
+
+    def initialize(geohash=nil)
+      self.geohash = geohash
+    end
   end
 end
