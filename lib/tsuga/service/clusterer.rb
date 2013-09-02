6ab499e80336efee0a40b1ba985c7d5692fbdf88
Julien Letessier <julien.letessier@gmail.com>
2013-09-02 23:39:54 +0100

    Completes first working clusterer & specs

--- lib/tsuga/service/clusterer.rb
+++ lib/tsuga/service/clusterer.rb
@@ -5,6 +5,7 @@ module Tsuga::Service
   class Clusterer
     MAX_DEPTH = 20
     MIN_DEPTH = 3
+    Tile = Tsuga::Model::Tile
 
     attr_reader :_adapter
 
@@ -18,9 +19,9 @@ module Tsuga::Service
 
       # for all depths N from 20 to 3
       MAX_DEPTH.downto(MIN_DEPTH) do |depth|
-        # create clusters from lower-level clusters (or records)
-        # 1 tile processed at each iteractions.
+        log "at depth #{depth}"
 
+        # find children (clusters or records) from deeper level, N+!
         if depth == MAX_DEPTH
           points_ids = _adapter.records.collect_ids
           find_from = _adapter.records
@@ -29,72 +30,95 @@ module Tsuga::Service
           find_from = _adapter.clusters
         end
 
+        if points_ids.empty?
+          log "nothing to cluster"
+          return
+        end
+
+        # for all children at depth N+1 (records if deepest level)
+        # create a cluster of level N pointing to the child.
         # assuming the data set is sparse, we walk the set instead of walking
         # all possible tiles.
         # 1 tile processed at each iteractions.
+        log "creating clusters from children"
         while points_ids.any?
-          points = find_from.find(points_ids.first)
+          log "... #{points_ids.size} children left"
+          point = find_from.find(points_ids.first)
           tile = Tile.including(point, :depth => depth)
           used_ids = _create_clusters(tile)
           points_ids -= used_ids
         end
+        log "... done"
 
+        # with the same sparse-wlk logic, run aggregation in each tile
+        # containing clusters.
         cluster_ids = _adapter.clusters.at_depth(depth).collect_ids
+        log "aggregating clusters"
         while cluster_ids.any?
+          log "... #{cluster_ids.size} clusters to process"
           cluster = _adapter.clusters.find(cluster_ids.first)
           tile = Tile.including(cluster, :depth => depth)
-          _assemble_clusters(tile)
+          used_ids = _assemble_clusters(tile)
+          cluster_ids -= used_ids
         end
+        log "... done"
 
-        _walk_tiles_at(depth) do |tile|
-          # for all clusters at depth N+1 in tile T (records if deepest level)
-          # create a cluster of level N pointing to the deeper cluster
-          _create_clusters(depth, tile)
-          # run clustering in tile
-          _assemble_clusters(tile)
-        end
-
-        # for each tile T
-        _walk_tiles_at(depth) do |tile|
-          # run clustering with this tile's and the neighbouringh tiles's clusters
-          # _assemble_clusters(*tile.neighbours)
-        end
+        # not implemented yet:
+        # run clustering with this tile's and the neighbouringh tiles's clusters
+        # _assemble_clusters(*tile.neighbours)
+      end
     end
 
     private
 
-    # shorthand
-    Tile = Tsuga::Model::Tile
+
+    def log(msg)
+      return unless ENV['VERBOSE']
+      $stderr.puts("[clusterer] #{msg}")
+      $stderr.flush
+    end
 
 
     # return the record IDs used
     def _create_clusters(tile)
-      result = Set.new
+      used_ids = []
 
       if tile.depth == MAX_DEPTH
         _adapter.records
-        .in_tile(tile).find_each do |record|
-          cluster = _adaper.clusters.new(depth, record)
+        .in_tile(tile).find_each do |child|
+          cluster = _adapter.clusters.build_from(tile.depth, child)
           cluster.persist!
-          result << cluster.id
+          used_ids << child.id
         end
       else
         _adapter.clusters.at_depth(tile.depth+1)
-        .in_tile(tile).find_each do |cluster|
-          parent = _adaper.clusters.new(depth, cluster)
-          parent.persist!
-          result << cluster.id
-
-          cluster.parent_id = parent.id
+        .in_tile(tile).find_each do |child|
+          cluster = _adapter.clusters.build_from(tile.depth, child)
           cluster.persist!
+
+          child.parent_id = child.id
+          child.persist!
+          used_ids << child.id
         end
       end
+
+      return used_ids
     end
 
+
     def _assemble_clusters(*tiles)
-      clusters = Set.new
-      _adapter.clusters.in_tile(*tiles).find_each { |c| clusters << c }
+      raise ArgumentError if tiles.map(&:depth).sort.uniq.size > 1
+
+      clusters = []
+      depth = tiles.first.depth
+
+      _adapter.clusters
+        .at_depth(depth)
+        .in_tile(*tiles)
+        .find_each { |c| clusters << c }
+      used_ids = clusters.map(&:id)
       Aggregator.new(clusters).run
+      used_ids
     end
 
   end
---
+++ spec/service/clusterer_spec.rb
@@ -0,0 +1,83 @@
+require 'spec_helper'
+require 'tsuga/service/clusterer'
+require 'tsuga/adapter/memory_adapter'
+
+describe Tsuga::Service::Clusterer do
+  subject { described_class.new(adapter) }
+  let(:adapter) { Tsuga::Adapter::MemoryAdapter.new  }
+
+  def make_record(lat, lng)
+    adapter.records.new.set_coords(lat,lng).persist!
+  end
+
+  before { adapter.clusters.delete_all }
+  before { adapter.records.delete_all }
+
+  describe '#run' do
+    context 'with no records' do
+      it('passes') { subject.run }
+    end
+
+    context 'with a single record' do
+      before do
+        make_record(0,0)
+      end
+
+      it('passes') { subject.run }
+
+      it 'creates 1 cluster per depth' do
+        subject.run
+        adapter.clusters.collect_ids.length.should == 18
+      end
+
+      it 'creates clusters with the same position' do
+        subject.run
+        hashes = Set.new
+        adapter.clusters.find_each { |c| hashes << c.geohash }
+        hashes.size.should == 1
+      end
+    end
+
+    context 'with two distant records' do
+      before do
+        make_record(0,0)
+        make_record(45,0)
+      end
+
+      it('passes') { subject.run }
+
+      it 'creates 2 clusters per depth' do
+        subject.run
+        adapter.clusters.collect_ids.length.should == 36
+      end
+    end
+
+
+    context 'with random records' do
+      before do
+        10.times { make_record(rand, rand) }
+        subject.run
+      end
+
+      let :toplevel_cluster do
+        id = adapter.clusters.at_depth(3).collect_ids.first
+        adapter.clusters.find(id)
+      end
+
+      let :barycenter do
+        sum_lat = 0
+        sum_lng = 0
+        adapter.records.find_each { |r| sum_lat += r.lat ; sum_lng += r.lng }
+        Tsuga::Model::Point.new.set_coords(0.1 * sum_lat, 0.1 * sum_lng)
+      end
+
+      it 'toplevel cluster has correct weight' do
+        toplevel_cluster.weight.should == 10
+      end
+
+      it 'toplevel cluster is centered' do
+        toplevel_cluster.should == barycenter
+      end
+    end
+  end
+end
