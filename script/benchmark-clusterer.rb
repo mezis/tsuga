7f00a5960b0b8d285d4c8d69c2dc99069a97446a
Julien Letessier <julien.letessier@gmail.com>
2013-09-08 23:24:05 +0100

    Permits benchmarking with different datasets

--- script/benchmark-clusterer.rb
+++ script/benchmark-clusterer.rb
@@ -5,16 +5,20 @@ require 'perftools'
 require 'benchmark'
 require 'zlib'
 require 'yaml'
+require 'csv'
+require 'ostruct'
 require 'tsuga/adapter/memory_adapter'
 require 'tsuga/adapter/sequel_adapter'
 require 'tsuga/adapter/mongoid_adapter'
 require 'tsuga/service/clusterer'
 
-Limit = ENV.fetch('LIMIT', '200').to_i
-
 ENV['CPUPROFILE_FREQUENCY'] ||= '500'
 
-case ENV['ADAPTER']
+LIMIT = ENV.fetch('LIMIT', '200').to_i
+SOURCE = ENV.fetch('SOURCE', 'doc/barcelona.csv.gz')
+ADAPTER_NAME = ENV.fetch('ADAPTER','mysql')
+
+case ADAPTER_NAME
 when /memory/i
   Adapter = Tsuga::Adapter::MemoryAdapter.new
 when /mysql/i
@@ -30,36 +34,36 @@ Clusters = Adapter.clusters
 Records  = Adapter.records
 
 puts 'loading records...'
-raw = File.open('doc/places.yml.gz', 'r') { |io| Zlib::GzipReader.new(io).read }
-data = YAML.load(raw)
+data = {}
+Zlib::GzipReader.open(SOURCE) do |io|
+  CSV(io) do |csv|
+    csv.each do |row|
+      id,lng,lat = row
+      data[id] = {lat:lat.to_f, lng:lng.to_f}
+      break if data.size >= LIMIT
+    end
+  end
+end
 
 puts 'creating records...'
 Records.delete_all
-total = 0
 data.each_pair do |k,v|
   Records.new.set_coords(v[:lat], v[:lng]).persist!
-  # r = Records.new.set_coords(rand, rand)
-  # require 'pry' ; require 'pry-nav' ; binding.pry
-  # r.persist!
-  total += 1
-  break if total >= Limit
 end
 puts " #{Records.count} records created"
 
-# require 'pry' ; require 'pry-nav' ; binding.pry
-
 puts 'profiling...'
-# PerfTools::CpuProfiler.start("tmp/profile") do
+PerfTools::CpuProfiler.start("tmp/profile") do
   Tsuga::Service::Clusterer.new(Adapter).run
-# end
+end
 
-# system "pprof.rb --pdf tmp/profile > tmp/profile.pdf"
-# system "open tmp/profile.pdf"
+system "pprof.rb --pdf tmp/profile > tmp/profile.pdf"
+system "open tmp/profile.pdf"
 
 
 __END__
 
-100,000 records:
+100,000 random records:
   real  110m17.156s
   user  83m0.333s
   sys 8m34.427s
