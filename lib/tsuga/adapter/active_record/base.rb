5412b65ce9c267c472e2776cf56b2a429f38f441
Julien Letessier <julien.letessier@gmail.com>
2013-10-28 11:26:56 +0000

    Works around old version of SQLite on Travis

--- lib/tsuga/adapter/active_record/base.rb
+++ lib/tsuga/adapter/active_record/base.rb
@@ -20,7 +20,29 @@ module Tsuga::Adapter::ActiveRecord
     module DatasetMethods
       def mass_create(new_records)
         return if new_records.empty?
-        attributes = new_records.map(&:attributes)
+
+        # Old SQLite versions (like on Travis) do not support bulk inserts
+        if connection.class.name !~ /sqlite/i || connection.send(:sqlite_version) >= '3.7.11'
+          _bulk_insert(new_records)
+        else
+          new_records.each(&:save!)
+        end
+      end
+
+      def mass_update(records)
+        transaction do
+          records.each(&:save!)
+        end
+      end
+
+      def collect_ids
+        pluck(:id)
+      end
+
+      private
+
+      def _bulk_insert(records)
+        attributes = records.map(&:attributes)
         keys = attributes.first.keys - ['id']
         column_names = keys.map { |k| connection.quote_column_name(k) }.join(', ')
         sql = <<-SQL
@@ -34,17 +56,6 @@ module Tsuga::Adapter::ActiveRecord
         full_sql = sql + value_strings.map { |str| "(#{str})"}.join(', ')
         connection.insert_sql(full_sql)
       end
-
-      def mass_update(records)
-        transaction do
-          records.each(&:save!)
-        end
-      end
-
-      def collect_ids
-        pluck(:id)
-      end
-
     end
   end
 end
